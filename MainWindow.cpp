d20cfd74b2f208aa05e660dd64ad1024c7d6efc8
habi <habijung0@gmail.com>
2024-03-23 20:20:50 +0900

    feat: QPushButton, QMessageBox 연습
    - Main window에 들어가는게 아니라 새로운 윈도우로 생성됨

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,9 +1,25 @@
 #include "MainWindow.h"
+#include "qpushbutton.h"
+#include "qmessagebox.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+
+    // QPushButton Example
+    QPushButton btn;
+    btn.setText("QPushButton");
+    btn.setToolTip("A tooltip");
+    btn.show();
+
+    // QMessageBox Example
+    QMessageBox msg;
+    msg.setText("QMessageBox");
+    msg.setInformativeText("Any changes?");
+    msg.setStandardButtons(QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel);
+    msg.setDefaultButton(QMessageBox::Save);
+    int ret = msg.exec();
 }
 
 MainWindow::~MainWindow()

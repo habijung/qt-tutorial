241fff1f295188d6a446fc16fdca978b6d98aead
habi <habijung0@gmail.com>
2024-03-23 20:21:16 +0900

    feat: QPushButton을 Central Widget으로 설정하기

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -7,19 +7,10 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui.setupUi(this);
 
-    // QPushButton Example
-    QPushButton btn;
-    btn.setText("QPushButton");
-    btn.setToolTip("A tooltip");
-    btn.show();
-
-    // QMessageBox Example
-    QMessageBox msg;
-    msg.setText("QMessageBox");
-    msg.setInformativeText("Any changes?");
-    msg.setStandardButtons(QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel);
-    msg.setDefaultButton(QMessageBox::Save);
-    int ret = msg.exec();
+    // Create button with central widget
+    QPushButton* btn = new QPushButton;
+    btn->setText("Button");
+    setCentralWidget(btn);
 }
 
 MainWindow::~MainWindow()

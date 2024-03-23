b0001a413eb4e7a3e81712b4ab5a0af13cd74448
habi <habijung0@gmail.com>
2024-03-23 20:21:29 +0900

    feat: Delegate에서 ButtonClicked Signal 활용하기

--- main.cpp
+++ main.cpp
@@ -2,20 +2,26 @@
 #include "Model.h"
 #include "Delegate.h"
 #include <QtWidgets/QApplication>
+#include <QObject>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.setWindowTitle("MainWindow");
-    w.show();
+	QApplication a(argc, argv);
+	MainWindow w;
+	w.setWindowTitle("MainWindow");
+	w.show();
 
-    QListView view;
-    Model model;
-    Delegate delegate;
-    view.setModel(&model);
-    view.setItemDelegate(&delegate);
-    view.show();
+	QListView view;
+	Model model;
+	Delegate delegate;
 
-    return a.exec();
+	QObject::connect(&delegate, &Delegate::ButtonClicked, [](QModelIndex index) {
+		qDebug() << index.row();
+		});
+
+	view.setModel(&model);
+	view.setItemDelegate(&delegate);
+	view.show();
+
+	return a.exec();
 }

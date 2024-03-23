396ff4879c1db4f05b1219553c9783d319dd5f4a
habi <habijung0@gmail.com>
2024-03-23 20:21:21 +0900

    feat: 새로운 위젯을 central widget으로 설정하고 QVBoxLayout으로 정렬

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,6 +1,7 @@
 #include "MainWindow.h"
 #include "qpushbutton.h"
 #include "qmessagebox.h"
+#include "qboxlayout.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -8,9 +9,14 @@ MainWindow::MainWindow(QWidget *parent)
     ui.setupUi(this);
 
     // Create button with central widget
+    QWidget* widget = new QWidget;
+    QVBoxLayout* vlay = new QVBoxLayout(widget);
     QPushButton* btn = new QPushButton;
+
     btn->setText("Button");
-    setCentralWidget(btn);
+    vlay->addWidget(btn);
+    widget->setLayout(vlay);
+    setCentralWidget(widget);
 }
 
 MainWindow::~MainWindow()
